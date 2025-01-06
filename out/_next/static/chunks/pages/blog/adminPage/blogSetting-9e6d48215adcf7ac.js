29d4d7caee1a3f0d90b0a42abb4f248ba973a12e
SachinchobeyRedsky <sachinchobey.redskyatech@gmail.com>
2025-01-06 15:09:23 +0530

    Make some dependencies changes

---
+++ out/_next/static/chunks/pages/blog/adminPage/blogSetting-9e6d48215adcf7ac.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2996],{82428:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/adminPage/blogSetting",function(){return u(86799)}])}},function(n){n.O(0,[2016,1551,9260,8646,9315,5357,9876,9839,7761,653,4521,7793,6799,9774,2888,179],(function(){return _=82428,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
