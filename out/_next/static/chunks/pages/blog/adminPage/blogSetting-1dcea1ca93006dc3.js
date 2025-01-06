5d5432d52a24c3061083810963969a37f2940884
SachinchobeyRedsky <sachinchobey.redskyatech@gmail.com>
2025-01-06 12:47:23 +0530

    new resolved issues build

--- out/_next/static/chunks/pages/blog/adminPage/blogSetting-1dcea1ca93006dc3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2996],{82428:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/adminPage/blogSetting",function(){return u(86799)}])}},function(n){n.O(0,[2016,4597,5675,8646,1759,3238,9876,9839,5048,653,4521,7793,6799,9774,2888,179],(function(){return _=82428,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
